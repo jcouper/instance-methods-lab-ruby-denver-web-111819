eabf0258c4904fc8bb59cee74d019f5951298f73
jcouper <joshcouper@gmail.com>
2020-02-20 13:47:29 -0700

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+# Person
+#   is defined (FAILED - 1)
+#   #talk
+#     responds to the #talk instance method (FAILED - 2)
+#     puts "Hello World!" (FAILED - 3)
+#   #walk
+#     responds to the #walk instance method (FAILED - 4)
+#     puts "The Person is walking" (FAILED - 5)
+
+
+class Person 
+
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk 
+        puts "The Person is walking"
+    end
+
+end
